ed92ec57613dc75d6a72a2dbcacebaa25a797ea6
WGeckle80 <75913247+WGeckle80@users.noreply.github.com>
2022-07-28 16:49:36 -0400

    Added polar coordinate lines and arrows
    Changed some required parameter names

--- reportlab_canvas_extension.py
+++ reportlab_canvas_extension.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Wyatt Geckle
-# 7/14/22
+# 7/28/22
 
 """Define extra methods or extend functionality of existing ones in the
    ReportLab Canvas class.
@@ -69,11 +69,11 @@ class Canvas(StockCanvas):
         self.restoreState()
         
     def arrowAngle(
-            self, x1: float, y1: float, length: float, theta: float,
+            self, x1: float, y1: float, r: float, theta: float,
             radians: bool = False,
             arrowheadlength: float = None) -> (float, float):
-        """Draw an arrow from (x1, y1) to polar coordinates (r, theta)
-        with arrowhead at (r, theta).
+        """Draw an arrow from (x1, y1) to polar coordinates
+        (r, theta) with arrowhead at (r, theta).
         
         theta is in degrees by default.
         
@@ -91,7 +91,7 @@ class Canvas(StockCanvas):
         if not radians:
             theta = math.radians(theta)
             
-        x2, y2 = self.lineAngle(x1, y1, length, theta, radians=True)
+        x2, y2 = self.lineAngle(x1, y1, r, theta, radians=True)
         
         if arrowheadlength is None:
             arrowheadlength = 0.083333 * math.sqrt((x2 - x1)*(x2 - x1)
@@ -103,6 +103,76 @@ class Canvas(StockCanvas):
                        radians=True)
                        
         self.restoreState()
+
+        return x2, y2
+
+    def arrowPolar(
+            self, x: float, y: float, r1: float, theta1: float,
+            r2: float, theta2: float, radians: bool = False,
+            arrowheadlength: float = None) -> (float, float):
+        """Draw a line segment from polar coordinates (r1, theta1) to
+        (r1 + dr, theta1 + dtheta), with (x, y) being the reference point.
+
+        Angles are in degrees by default.
+        
+        Arrowhead size is about 1/12 of the arrow length by default.
+        
+        Returns the x and y coordinates of (r2, theta2)."""
+
+        if not radians:
+            theta1 = math.radians(theta1)
+            theta2 = math.radians(theta2)
+
+        x1 = r1*math.cos(theta1) + x
+        y1 = r1*math.sin(theta1) + y
+
+        x2 = r2*math.cos(theta2) + x
+        y2 = r2*math.sin(theta2) + y
+
+        self.arrow(x1, y1, x2, y2, arrowheadlength=arrowheadlength)
+
+        return x2, y2
+    
+    def arrowPolarRelative(
+            self, x: float, y: float, r1: float, theta1: float,
+            dr: float, dtheta: float, radians: bool = False,
+            arrowheadlength: float = None) -> (float, float):
+        """Draw an arrow from polar coordinates (r1, theta1) to
+        (r2, theta2), with (x, y) being the reference point.
+
+        Angles are in degrees by default.
+        
+        Arrowhead size is about 1/12 of the arrow length by default.
+        
+        Returns the x and y coordinates of the arrow endpoint."""
+
+        if not radians:
+            theta1 = math.radians(theta1)
+            theta2 = math.radians(theta2)
+
+        x1 = r1*math.cos(theta1) + x
+        y1 = r1*math.sin(theta1) + y
+
+        return self.arrowAngle(x1, y1, dr, dtheta, radians=True,
+                               arrowheadlength=arrowheadlength)
+
+    def arrowRelative(
+            self, x1: float, y1: float, dx: float, dy: float,
+            arrowheadlength: float = None) -> (float, float):
+        """Draw an arrow from (x1, y1) to (x1 + dx, y1 + dy) with
+        arrowhead at (x1 + dx, y1 + dy).
+        
+        Arrowhead size is about 1/12 of the arrow length by default.
+        
+        Due to issues with the default end, the line cap is always
+        round.
+
+        Returns the x and y coordinates of the arrow endpoint."""
+
+        x2 = x1 + dx
+        y2 = y1 + dy
+
+        self.arrow(x1, y1, x2, y2, arrowheadlength=arrowheadlength)
 
         return x2, y2
                    
@@ -205,22 +275,200 @@ class Canvas(StockCanvas):
         self.drawCentredString(x, y - y_offset, text, **kwargs)
                    
     def lineAngle(
-            self, x1: float, y1: float, length: float, theta: float,
+            self, x1: float, y1: float, r: float, theta: float,
             radians: bool = False) -> (float, float):
         """Draw a line segment from (x1, y1) to polar coordinates
         (r, theta).
         
         theta is in degrees by default.
 
-        Returns the x and y coordinates of the line endpoint."""
+        Returns the x and y coordinates of the second point."""
         
         if not radians:
             theta = math.radians(theta)
             
-        x2 = length * math.cos(theta) + x1
-        y2 = length * math.sin(theta) + y1
+        x2 = r*math.cos(theta) + x1
+        y2 = r*math.sin(theta) + y1
         
         self.line(x1, y1, x2, y2)
+
+        return x2, y2
+
+    def lineAngleDashed(
+            self, x1: float, y1: float, r: float, theta: float,
+            radians: bool = False, num_dashes: int = None) -> (float, float):
+        """Draw a dashed line from (x1, y1) to polar coordinates
+        (r, theta)
+
+        theta is in degrees by default.
+
+        num_dashes depends on line length by default.
+
+        Returns the x and y coordinates of the second point."""
+
+        if not radians:
+            theta = math.radians(theta)
+
+        if num_dashes is None:
+            # num_dashes should be at least 2.
+            num_dashes = int(abs(r) / 30) + 2
+
+        dash_length = r / (num_dashes*2 - 1)
+        dash_base = dash_length * math.cos(theta)
+        dash_height = dash_length * math.sin(theta)
+
+        for i in range(num_dashes):
+            self.lineRelative(2*i*dash_base + x1,
+                              2*i*dash_height + y1,
+                              dash_base,
+                              dash_height)
+
+        return ((2*num_dashes - 1)*dash_base + x1,
+                (2*num_dashes - 1)*dash_height + y1)
+
+    def lineDashed(
+            self, x1: float, y1: float, x2: float, y2: float,
+            num_dashes: int = None):
+        """Draw a dashed line from (x1, y1) to (x2, y2).
+
+        num_dashes depends on line length by default."""
+
+        if num_dashes is None:
+            length = math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1))
+            # num_dashes should be at least 2.
+            num_dashes = int(length / 30) + 2
+
+        dash_base = (x2 - x1) / (num_dashes*2 - 1)
+        dash_height = (y2 - y1) / (num_dashes*2 - 1)
+
+        for i in range(num_dashes):
+            self.lineRelative(2*i*dash_base + x1,
+                              2*i*dash_height + y1,
+                              dash_base,
+                              dash_height)
+
+    def linePolar(
+            self, x: float, y: float, r1: float, theta1: float,
+            r2: float, theta2: float, radians: bool = False) -> (float, float):
+        """Draw a line segment from polar coordinates (r1, theta1) to
+        (r2, theta2), with (x, y) being the reference point.
+
+        Angles are in degrees by default.
+        
+        Returns the x and y coordinates of (r2, theta2)."""
+
+        if not radians:
+            theta1 = math.radians(theta1)
+            theta2 = math.radians(theta2)
+
+        x1 = r1*math.cos(theta1) + x
+        y1 = r1*math.sin(theta1) + y
+
+        x2 = r2*math.cos(theta2) + x
+        y2 = r2*math.sin(theta2) + y
+
+        self.line(x1, y1, x2, y2)
+
+        return x2, y2
+
+    def linePolarDashed(
+            self, x: float, y: float, r1: float, theta1: float,
+            r2: float, theta2: float, radians: bool = False,
+            num_dashes: int = None) -> (float, float):
+        """Draw a dashed line from polar coordinates (r1, theta1) to
+        (r2, theta2), with (x, y) being the reference point.
+
+        Angles are in degrees by default.
+
+        num_dashes depends on line length by default.
+
+        Returns the x and y coordinates of (r2, theta2)."""
+
+        if not radians:
+            theta1 = math.radians(theta1)
+            theta2 = math.radians(theta2)
+
+        x1 = r1*math.cos(theta1) + x
+        y1 = r1*math.sin(theta1) + y
+
+        x2 = r2*math.cos(theta2) + x
+        y2 = r2*math.sin(theta2) + y
+
+        self.lineDashed(x1, y1, x2, y2, num_dashes=num_dashes)
+
+        return x2, y2
+    
+    def linePolarRelative(
+            self, x: float, y: float, r1: float, theta1: float,
+            dr: float, dtheta: float, radians: bool = False) -> (float, float):
+        """Draw a line segment from polar coordinates (r1, theta1) to
+        (r1 + dr, theta1 + dtheta), with (x, y) being the reference
+        point.
+
+        Angles are in degrees by default.
+        
+        Returns the x and y coordinates of the second point."""
+
+        if not radians:
+            theta1 = math.radians(theta1)
+            dtheta = math.radians(dtheta)
+
+        x1 = r1*math.cos(theta1) + x
+        y1 = r1*math.sin(theta1) + y
+
+        return self.lineAngle(x1, y1, dr, dtheta, radians=True)
+
+    def linePolarRelativeDashed(
+            self, x: float, y: float, r1: float, theta1: float,
+            dr: float, dtheta: float, radians: bool = False,
+            num_dashes: int = None) -> (float, float):
+        """Draw a line segment from polar coordinates (r1, theta1) to
+        (r1 + dr, theta1 + dtheta), with (x, y) being the reference
+        point.
+
+        Angles are in degrees by default.
+        
+        num_dashes depends on line length by default.
+        
+        Returns the x and y coordinates of the second point."""
+        
+        if not radians:
+            theta1 = math.radians(theta1)
+            dtheta = math.radians(dtheta)
+
+        x1 = r1*math.cos(theta1) + x
+        y1 = r1*math.sin(theta1) + y
+
+        return self.lineAngleDashed(x1, y1, dr, dtheta, radians=True,
+                                    num_dashes=num_dashes)
+
+    def lineRelative(
+            self, x1: float, y1: float,
+            dx: float, dy: float) -> (float, float):
+        """Draw a line segment from (x1, y1) to (x1 + dx, y1 + dy).
+
+        Returns the x and y coordinates of the second point."""
+
+        x2 = x1 + dx
+        y2 = y1 + dy
+
+        self.line(x1, y1, x2, y2)
+
+        return x2, y2
+
+    def lineRelativeDashed(
+            self, x1: float, y1: float, dx: float, dy: float,
+            num_dashes: int = None) -> (float, float):
+        """Draw a dashed line from (x1, y1) to (x1 + dx, y1 + dy).
+
+        num_dashes depends on line length by default.
+
+        Returns the x and y coordinates of the second point."""
+
+        x2 = x1 + dx
+        y2 = y1 + dy
+
+        self.lineDashed(x1, y1, x2, y2, num_dashes=num_dashes)
 
         return x2, y2
         
